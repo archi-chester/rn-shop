0a7d4b20854c53a72c352dca798b7b177237e481
Archi Chester <me@qz0.ru>
2020-06-15 17:57:55 +0300

    menu

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
 import * as cartActions from '../../store/actions/cart';
+import * as orderActions from '../../store/actions/orders';
 
 const CartScreen = (props) => {
     const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
@@ -29,7 +30,14 @@ const CartScreen = (props) => {
                 <Text style={styles.summaryText}>
                     Total: <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
                 </Text>
-                <Button color={Colors.accent} title="Order Now" disabled={cartItems.length === 0} />
+                <Button
+                    color={Colors.accent}
+                    title="Order Now"
+                    disabled={cartItems.length === 0}
+                    onPress={() => {
+                        dispatch(orderActions.addOrder(cartItems, cartTotalAmount));
+                    }}
+                />
             </View>
             <FlatList
                 data={cartItems}
@@ -47,6 +55,10 @@ const CartScreen = (props) => {
             />
         </View>
     );
+};
+
+CartScreen.navigationOptions = {
+    headerTitle: 'Your Cart',
 };
 
 const styles = StyleSheet.create({
