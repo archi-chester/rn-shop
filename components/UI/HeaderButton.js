6291b199a29356ece27a8f0add7605f977e8ae45
Archi Chester <me@qz0.ru>
2020-07-03 22:24:10 +0300

    last fix fegore auth

--- components/UI/HeaderButton.js
+++ components/UI/HeaderButton.js
@@ -5,15 +5,15 @@ import { Ionicons } from '@expo/vector-icons';
 
 import Colors from '../../constants/Colors';
 
-const CustomHeaderButton = (props) => {
-    return (
-        <HeaderButton
-            {...props}
-            IconComponent={Ionicons}
-            iconSize={23}
-            color={Platform.OS === 'android' ? 'white' : Colors.primary}
-        />
-    );
+const CustomHeaderButton = props => {
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Ionicons}
+      iconSize={23}
+      color={Platform.OS === 'android' ? 'white' : Colors.primary}
+    />
+  );
 };
 
 export default CustomHeaderButton;
